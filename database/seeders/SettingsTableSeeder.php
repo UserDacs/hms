91aba064236c2dbe628a9fb198495182aee33b7e
aljoe <yuriii@gmail.com>
2025-05-20 21:05:11 +0800

    revalue

--- database/seeders/SettingsTableSeeder.php
+++ database/seeders/SettingsTableSeeder.php
@@ -16,14 +16,14 @@ class SettingsTableSeeder extends Seeder
     {
         $imageUrl = ('web/img/logo.jpg');
 
-        Setting::create(['key' => 'app_name', 'value' => 'HMS']);
+        Setting::create(['key' => 'app_name', 'value' => 'BIS']);
         Setting::create(['key' => 'app_logo', 'value' => $imageUrl]);
-        Setting::create(['key' => 'company_name', 'value' => 'InfyOmLabs']);
-        Setting::create(['key' => 'current_currency', 'value' => 'inr']);
-        Setting::create(['key' => 'hospital_address', 'value' => '16/A saint Joseph Park']);
-        Setting::create(['key' => 'hospital_email', 'value' => 'cityhospital@gmail.com']);
-        Setting::create(['key' => 'hospital_phone', 'value' => '+919876543210']);
+        Setting::create(['key' => 'company_name', 'value' => 'ComLab']);
+        Setting::create(['key' => 'current_currency', 'value' => 'PHP']);
+        Setting::create(['key' => 'hospital_address', 'value' => 'Bohol']);
+        Setting::create(['key' => 'hospital_email', 'value' => 'barangay@gmail.com']);
+        Setting::create(['key' => 'hospital_phone', 'value' => '+639123456789']);
         Setting::create(['key' => 'hospital_from_day', 'value' => 'Mon to Fri']);
-        Setting::create(['key' => 'hospital_from_time', 'value' => '9 AM to 9 PM']);
+        Setting::create(['key' => 'hospital_from_time', 'value' => '8 AM to 5 PM']);
     }
 }
